6d66df2b2b1729e5fdf1b002643be9c3604a5eb0
Ben Pate <ben@pate.org>
2023-01-17 18:03:26 -0800

    Refactor sliceof.Type => sliceof.Object

---
+++ sliceof/object.go
@@ -0,0 +1,68 @@
+package sliceof
+
+import (
+	"github.com/benpate/rosetta/schema"
+)
+
+type Object[T any] []T
+
+/****************************************
+ * Accessors
+ ****************************************/
+
+func (x Object[T]) Length() int {
+	return len(x)
+}
+
+func (x Object[T]) IsLength(length int) bool {
+	return len(x) == length
+}
+
+func (x Object[T]) IsEmpty() bool {
+	return len(x) == 0
+}
+
+func (x Object[T]) First() T {
+	if len(x) > 0 {
+		return x[0]
+	}
+	var result T
+	return result
+}
+
+func (x Object[T]) Last() T {
+	if len(x) > 0 {
+		return x[len(x)-1]
+	}
+	var result T
+	return result
+}
+
+func (x Object[T]) Reverse() {
+	for i, j := 0, len(x)-1; i < j; i, j = i+1, j-1 {
+		x[i], x[j] = x[j], x[i]
+	}
+}
+
+/****************************************
+ * Tree Traversal
+ ****************************************/
+
+func (x *Object[T]) GetObject(name string) (any, bool) {
+
+	// Get a valid index for the slice
+	if index, ok := schema.Index(name); ok {
+
+		// Grow if necessary
+		for index >= len(*x) {
+			var newValue T
+			*x = append(*x, newValue)
+		}
+
+		// Return result
+		return &(*x)[index], true
+	}
+
+	// Failure!!
+	return nil, false
+}
