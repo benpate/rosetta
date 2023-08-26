441a71eb49cac025eb07f81eef1737aa7131fea4
Ben Pate <ben@pate.org>
2023-08-25 20:37:34 -0600

    Remove "done" parameter from channel.Reverse

--- channel/reverse.go
+++ channel/reverse.go
@@ -1,8 +1,8 @@
 package channel
 
 // Reverse reads an entire channel into a stack, then writes it back out in reverse order
-// Since it keeps the entire channel in memory, it should not be used for large data sets
-func Reverse[T any](input <-chan T, done <-chan struct{}) <-chan T {
+// Since it keeps the entire channel in memory, it should not be used for unbounded data sets
+func Reverse[T any](input <-chan T) <-chan T {
 
 	result := make(chan T)
 
@@ -16,22 +16,12 @@ func Reverse[T any](input <-chan T, done <-chan struct{}) <-chan T {
 		// Read all items from the input channel
 		for item := range input {
 
-			// Breakpoint for cancellation
-			if Closed(done) {
-				return
-			}
-
 			// Push the item onto the stack
 			items = append(items, item)
 		}
 
 		// Write all items back to the output channel
 		for i := len(items) - 1; i >= 0; i-- {
-
-			// Breakpoint for cancellation
-			if Closed(done) {
-				return
-			}
 
 			// Send the item to the caller
 			result <- items[i]
