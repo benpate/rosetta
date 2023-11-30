7f1f678c47cc5af33999e19f3abc0824694803bb
Ben Pate <ben@pate.org>
2023-11-29 22:32:32 -0700

    Add channel.Map function

---
+++ channel/map.go
@@ -0,0 +1,16 @@
+package channel
+
+// Map applies a mapping function to every value in the input channel, passing the mapped
+// values out to the output channel.
+func Map[Input any, Output any](input <-chan Input, mapper func(Input) Output) <-chan Output {
+
+	result := make(chan Output, 1)
+
+	go func() {
+		for value := range input {
+			result <- mapper(value)
+		}
+	}()
+
+	return result
+}
