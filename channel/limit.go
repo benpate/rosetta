01792913007f70c90dd60eda09b2b9db011d3431
Ben Pate <ben@pate.org>
2024-01-27 17:23:43 -0700

    Fix Limit tests

--- channel/limit.go
+++ channel/limit.go
@@ -3,7 +3,7 @@ package channel
 // Limit returns a channel that will receive at most the specified number of items from the input channel.
 // When the maximum is reached, Limit will close the "done" channel, to communicate to other goroutines
 // that they can stop sending items.
-func Limit[T any](maximum int, input <-chan T, done chan struct{}) <-chan T {
+func Limit[T any](maximum int, input <-chan T, done chan<- struct{}) <-chan T {
 
 	result := make(chan T)
 
@@ -11,6 +11,10 @@ func Limit[T any](maximum int, input <-chan T, done chan struct{}) <-chan T {
 
 		defer close(result)
 		defer close(done)
+
+		if maximum <= 0 {
+			return
+		}
 
 		for item := range input {
 
--- channel/limit_test.go
+++ channel/limit_test.go
@@ -6,6 +6,41 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestLimit0(t *testing.T) {
+
+	done := make(Done)
+	in := Limit(0, testChannel(), done)
+
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+}
+
+func TestLimit1(t *testing.T) {
+
+	done := make(Done)
+	in := Limit(1, testChannel(), done)
+
+	require.Equal(t, "Hello", <-in)
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+}
+
+func TestLimit2(t *testing.T) {
+
+	done := make(Done)
+	in := Limit(2, testChannel(), done)
+
+	require.Equal(t, "Hello", <-in)
+	require.Equal(t, "There", <-in)
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+}
 func TestLimit3(t *testing.T) {
 
 	done := make(Done)
@@ -50,14 +85,66 @@ func TestLimit5(t *testing.T) {
 	require.Equal(t, "", <-in)
 }
 
+func TestLimit0_Closed(t *testing.T) {
+
+	done := make(Done)
+	in := Limit(0, testChannel(), done)
+	counter := 0
+
+	for range in {
+		counter++
+	}
+
+	require.Equal(t, 0, counter)
+	require.True(t, Closed(done))
+	require.True(t, Closed(in))
+}
+
+func TestLimit1_Closed(t *testing.T) {
+
+	done := make(Done)
+	in := Limit(1, testChannel(), done)
+	counter := 0
+
+	for range in {
+		counter++
+	}
+
+	require.Equal(t, 1, counter)
+
+	require.True(t, Closed(done))
+	require.True(t, Closed(in))
+}
+
+func TestLimit2_Closed(t *testing.T) {
+
+	done := make(Done)
+	in := Limit(2, testChannel(), done)
+	counter := 0
+
+	for range in {
+		counter++
+	}
+
+	require.Equal(t, 2, counter)
+
+	require.True(t, Closed(done))
+	require.True(t, Closed(in))
+}
+
 func TestLimit3_Closed(t *testing.T) {
 
 	done := make(Done)
 	in := Limit(3, testChannel(), done)
+	counter := 0
 
-	require.Equal(t, "Hello", <-in)
-	require.Equal(t, "There", <-in)
-	require.Equal(t, "General", <-in)
+	for range in {
+		counter++
+	}
+
+	require.Equal(t, 3, counter)
+
+	require.True(t, Closed(done))
 	require.True(t, Closed(in))
 }
 
@@ -65,11 +152,15 @@ func TestLimit4_Closed(t *testing.T) {
 
 	done := make(Done)
 	in := Limit(4, testChannel(), done)
+	counter := 0
 
-	require.Equal(t, "Hello", <-in)
-	require.Equal(t, "There", <-in)
-	require.Equal(t, "General", <-in)
-	require.Equal(t, "Kenobi", <-in)
+	for range in {
+		counter++
+	}
+
+	require.Equal(t, 4, counter)
+
+	require.True(t, Closed(done))
 	require.True(t, Closed(in))
 }
 
@@ -77,10 +168,14 @@ func TestLimit5_Closed(t *testing.T) {
 
 	done := make(Done)
 	in := Limit(5, testChannel(), done)
+	counter := 0
 
-	require.Equal(t, "Hello", <-in)
-	require.Equal(t, "There", <-in)
-	require.Equal(t, "General", <-in)
-	require.Equal(t, "Kenobi", <-in)
+	for range in {
+		counter++
+	}
+
+	require.Equal(t, 4, counter)
+
+	require.True(t, Closed(done))
 	require.True(t, Closed(in))
 }
