d54e7bab39362831d3500235ad4a61fcb68de21b
Ben Pate <ben@pate.org>
2024-01-27 17:08:09 -0700

    Update Limit Tests

--- channel/limit_test.go
+++ channel/limit_test.go
@@ -14,7 +14,9 @@ func TestLimit3(t *testing.T) {
 	require.Equal(t, "Hello", <-in)
 	require.Equal(t, "There", <-in)
 	require.Equal(t, "General", <-in)
-	require.True(t, Closed(in))
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
 	require.Equal(t, "", <-in)
 }
 
@@ -27,10 +29,51 @@ func TestLimit4(t *testing.T) {
 	require.Equal(t, "There", <-in)
 	require.Equal(t, "General", <-in)
 	require.Equal(t, "Kenobi", <-in)
-	require.True(t, Closed(in))
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
 }
 
 func TestLimit5(t *testing.T) {
+
+	done := make(Done)
+	in := Limit(5, testChannel(), done)
+
+	require.Equal(t, "Hello", <-in)
+	require.Equal(t, "There", <-in)
+	require.Equal(t, "General", <-in)
+	require.Equal(t, "Kenobi", <-in)
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+	require.Equal(t, "", <-in)
+}
+
+func TestLimit3_Closed(t *testing.T) {
+
+	done := make(Done)
+	in := Limit(3, testChannel(), done)
+
+	require.Equal(t, "Hello", <-in)
+	require.Equal(t, "There", <-in)
+	require.Equal(t, "General", <-in)
+	require.True(t, Closed(in))
+}
+
+func TestLimit4_Closed(t *testing.T) {
+
+	done := make(Done)
+	in := Limit(4, testChannel(), done)
+
+	require.Equal(t, "Hello", <-in)
+	require.Equal(t, "There", <-in)
+	require.Equal(t, "General", <-in)
+	require.Equal(t, "Kenobi", <-in)
+	require.True(t, Closed(in))
+}
+
+func TestLimit5_Closed(t *testing.T) {
 
 	done := make(Done)
 	in := Limit(5, testChannel(), done)
