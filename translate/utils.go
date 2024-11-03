f0d24366b89ea2d9f58cf867922416a0e537b5d1
Ben Pate <ben@pate.org>
2024-11-03 11:36:59 -0700

    Improve error reporting

--- translate/utils.go
+++ translate/utils.go
@@ -8,57 +8,20 @@ import (
 	"github.com/benpate/rosetta/mapof"
 )
 
+// executeTemplate is just some syntax sugar to execute a template and return the result as a string
 func executeTemplate(t *template.Template, data any) string {
 	var buffer bytes.Buffer
 
 	if err := t.Execute(&buffer, data); err != nil {
-		derp.Report(err)
+		derp.Report(derp.Wrap(err, "rosetta.translate.executeTemplate", "Error executing template", data))
 		return ""
 	}
 
 	return buffer.String()
 }
 
+// toPlainMap returns a mapof.Any as a map[string]any.
+// It's defined here to make it easier to call in slice.Map operations
 func toPlainMap(value mapof.Any) map[string]any {
 	return value.MapOfAny()
 }
-
-/* These functions are unused for now.  Hiding them so that the linter doesn't lose its mind.
-func getArrayLength(value any) (int, bool) {
-
-	switch typed := value.(type) {
-	case []any:
-		return len(typed), true
-	case []mapof.Any:
-		return len(typed), true
-	case schema.LengthGetter:
-		return typed.Length(), true
-	default:
-		return 0, false
-	}
-}
-
-func getArrayIndex(value any, index int) (any, bool) {
-
-	switch typed := value.(type) {
-	case []any:
-		return typed[index], true
-	case []mapof.Any:
-		return typed[index], true
-	case schema.ArrayGetter:
-		return typed.GetIndex(index)
-	default:
-		return nil, false
-	}
-}
-
-func setArrayIndex(value any, index int, item any) bool {
-
-	switch typed := value.(type) {
-	case schema.ArraySetter:
-		return typed.SetIndex(index, item)
-	default:
-		return false
-	}
-}
-*/
