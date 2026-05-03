d24eeeb7d608b7e48a883e5a9b6b0ef1c72dd979
Ben Pate <ben@pate.org>
2026-05-03 09:50:15 -0600

    ranges: add FilterPointer function
    For filtering sets of large objects where you don't want to copy the whole value

--- ranges/filter.go
+++ ranges/filter.go
@@ -16,3 +16,18 @@ func Filter[T any](iterator iter.Seq[T], predicate func(T) bool) iter.Seq[T] {
 		}
 	}
 }
+
+// FilterPointer returns a new iterator that yields only the items from the input iterator
+// for which the predicate function returns true.
+func FilterPointer[T any](iterator iter.Seq[T], predicate func(*T) bool) iter.Seq[T] {
+
+	return func(yield func(T) bool) {
+		for item := range iterator {
+			if predicate(&item) {
+				if !yield(item) {
+					return
+				}
+			}
+		}
+	}
+}
