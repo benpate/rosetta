97be303ee8ac921b761699c0b88434bd714b0d23
Ben Pate <ben@pate.org>
2026-06-10 15:21:37 -0600

    Linting
    - remove dead code
    - compare on RUNES, not bytes

--- html/summary.go
+++ html/summary.go
@@ -9,20 +9,12 @@ package html
 // Summary returns the first few sentences of content from an HTML document
 func Summary(html string) string {
 
-	// Try to get just the first paragraph of content.
-	// result := firstParagraph.FindString(html)
-
-	// If there is no "paragraph" marker (e.g. this is plain text) then just use the original
-	// if result == "" {
-	//	result = html
-	// }
-
 	// Remove all tags and whitespace
 	result := CollapseWhitespace(RemoveTags(html))
 
-	// Truncate to 200 characters (plus ellipsis)
-	if len(result) > 200 {
-		result = result[:200] + "..."
+	// Truncate to 200 runes (plus ellipsis)
+	if runes := []rune(result); len(runes) > 200 {
+		result = string(runes[:200]) + "..."
 	}
 
 	return result
