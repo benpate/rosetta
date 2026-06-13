fae154e755f922a5ec369f68660cd0c705e75896
Ben Pate <ben@pate.org>
2026-06-13 02:26:26 -0600

    Bugfix

--- slice/difference.go
+++ slice/difference.go
@@ -3,7 +3,7 @@ package slice
 func Difference[T comparable](a []T, b []T) []T {
 
 	// Create a slice to store the difference
-	result := make([]T, len(a))
+	result := make([]T, 0, len(a))
 
 	// Iterate over a and add elements not in b to the diff slice
 	for _, item := range a {
